ab3f1a705b63d51469f97ed7ed49e849e1769c7b
kangsi <398956601@qq.com>
2017-11-06 11:34:47 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -8,10 +8,10 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    index: path.resolve(__dirname, '../dist/static/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist/static'),
+    assetsSubDirectory: '',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -32,11 +32,11 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-        '/api': {
+        '/sell': {
             target: 'http://192.168.9.102:8080/sell',//设置你调用的接口域名和端口号 别忘了加http
             changeOrigin: true,
             pathRewrite: {
-            '^/api': '/'//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+            '^/sell': '/'//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
             }
         }
     },
--- src/api/index.js
+++ src/api/index.js
@@ -1,33 +1,33 @@
-import axios from 'axios'
+import axios from 'axios' 
 
-export const reqUserInfo = params => { return axios.get(`/api/ebay/user/account/${params.id}`, params) }
+export const reqUserInfo = params => { return axios.get(`/sell/ebay/user/account/${params.id}`, params) }
 //我的钱包
-export const reqMyWallet = params => { return axios.get(`/api/ebay/user/user/myWallet`, {params: params}) }
-export const reqMyWalletCount = params => { return axios.get(`/api/ebay/user/user/myWallet/count`, {params: params}) }
+export const reqMyWallet = params => { return axios.get(`/sell/ebay/user/user/myWallet`, {params: params}) }
+export const reqMyWalletCount = params => { return axios.get(`/sell/ebay/user/user/myWallet/count`, {params: params}) }
 //我的订单列表
-export const reqBuyerOrderList = params => { return axios.get(`/api/ebay/user/user/myOrder`, {params: params}) }
-export const reqBuyerOrderCreate = params => { return axios.post(`/api/buyer/order/create`, params) }
+export const reqBuyerOrderList = params => { return axios.get(`/sell/ebay/user/user/myOrder`, {params: params}) }
+export const reqBuyerOrderCreate = params => { return axios.post(`/sell/buyer/order/create`, params) }
 
 //购物车
-export const reqAddToShoppingCart = params => { return axios.post(`/api/buyer/goodcar/add/${params.userId}`, params.goodCarForm) }
-export const reqShoppingCartList = params => { return axios.get(`/api/buyer/goodcar/list`, {params: params}) }
-export const reqCartDelete = params => { return axios.delete(`/api/buyer/goodcar/delete/${params.productId}/${params.userId}`) }
+export const reqAddToShoppingCart = params => { return axios.post(`/sell/buyer/goodcar/add/${params.userId}`, params.goodCarForm) }
+export const reqShoppingCartList = params => { return axios.get(`/sell/buyer/goodcar/list`, {params: params}) }
+export const reqCartDelete = params => { return axios.delete(`/sell/buyer/goodcar/delete/${params.productId}/${params.userId}`) }
 
 
-export const reqSellerProList = params => { return axios.get(`/api/seller/product/list`, {params: params}) }
+export const reqSellerProList = params => { return axios.get(`/sell/seller/product/list`, {params: params}) }
 
-export const reqAddressList = params => { return axios.get(`/api/ebay/user/userCnee/list`, {params: params}) }
-export const reqAddressCreate = params => { return axios.post(`/api/ebay/user/userCnee/create`, params) }
-export const reqAddressDelete = params => { return axios.post(`/api/ebay/user/userCnee/${params.id}/update`, params) }
+export const reqAddressList = params => { return axios.get(`/sell/ebay/user/userCnee/list`, {params: params}) }
+export const reqAddressCreate = params => { return axios.post(`/sell/ebay/user/userCnee/create`, params) }
+export const reqAddressDelete = params => { return axios.post(`/sell/ebay/user/userCnee/${params.id}/update`, params) }
 
-export const reqMyCusCount = params => { return axios.get(`/api/ebay/user/user/myCustomerCount`, {params: params}) }
-export const reqMyCustomerList = params => { return axios.get(`/api/ebay/user/user/myCustomer`, {params: params}) }
+export const reqMyCusCount = params => { return axios.get(`/sell/ebay/user/user/myCustomerCount`, {params: params}) }
+export const reqMyCustomerList = params => { return axios.get(`/sell/ebay/user/user/myCustomer`, {params: params}) }
 
 
 //成为分销商
-export const reqUserUpdate = params => { return axios.post(`/api/ebay/user/${params.id}/update`, params) }
+export const reqUserUpdate = params => { return axios.post(`/sell/ebay/user/${params.id}/update`, params) }
 
-export const reqProductList = params => { return axios.get(`/api/buyer/product/list`,{params: params}) }
-export const reqProductDetail = params => { return axios.get(`/api/buyer/product/detail`, {params: params}) }
+export const reqProductList = params => { return axios.get(`/sell/buyer/product/list`,{params: params}) }
+export const reqProductDetail = params => { return axios.get(`/sell/buyer/product/detail`, {params: params}) }
 
-export const reqSellerProductSave = params => { return axios.post(`/api/seller/product/save`, params) }
+export const reqSellerProductSave = params => { return axios.post(`/sell/seller/product/save`, params) }
